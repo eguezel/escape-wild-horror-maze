b6a0fb0cae6004dd576f904e3facb98ec73c1eca
Hotaru24 <florent.pasquet.24@gmail.com>
2019-10-31 04:13:49 +0100

    bouton transition menu -> jeu fonctionne !

--- src/Components/TitleSCene/TitleScene.js
+++ src/Components/TitleSCene/TitleScene.js
@@ -1,3 +1,4 @@
+
 class TitleScene extends Phaser.Scene {
 
 	constructor() {
@@ -9,10 +10,21 @@ class TitleScene extends Phaser.Scene {
 	}
 
 	create() {
-		 var bg = this.add.sprite(0,0,'background');
+           
+        let bg = this.add.sprite(0,0,'background');
 		  bg.setOrigin(0,0);
 
-		  var text = this.add.text(100,100, 'Run for your life!');
+        let text = this.add.text(100,100, 'Run for your life!');
+            
+
+        text.setInteractive();
+        text.once('pointerup', function () {
+
+            this.scene.start('gameScene');
+
+        }, this);
+        
+        
 	}
 
 	update() {
